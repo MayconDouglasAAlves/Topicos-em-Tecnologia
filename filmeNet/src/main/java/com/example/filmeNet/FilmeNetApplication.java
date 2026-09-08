34dee36f0057a8407183d43067d63e19643ff503
MayconDouglasAAlves <maycond.jobs@gmail.com>
2026-09-07 21:00:19 -0300

    Começar o projeto proposto em aula

---
+++ filmeNet/src/main/java/com/example/filmeNet/FilmeNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmeNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeNetApplication.class, args);
+	}
+
+}
---
+++ filmeNet/src/test/java/com/example/filmeNet/FilmeNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmeNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
